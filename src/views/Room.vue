bf6f437ee12facf668e3d5c825985bf540a83b16
Marco Lin <pohanlin0605@gmail.com>
2021-08-23 10:26:21 +0800

    feat: add Room view

---
+++ src/views/Room.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="room__container">
+    <div class="room__image__wrapper">
+      <Navbar />
+    </div>
+    <div class="room__information__item">
+      <router-link to="/room/detail" class="room__information__link">
+        <div class="room__information__button">
+          房間介紹
+        </div>
+      </router-link>
+      <router-link to="/room/review" class="room__information__link">
+        <div class="room__information__button">
+          房客評價(128)
+        </div>
+      </router-link>
+    </div>
+    <RoomDetail />
+  </div>
+</template>
+
+<script>
+import Navbar from './../components/Navbar.vue'
+import RoomDetail from './../components/RoomDetail.vue'
+export default {
+  name: 'Room',
+  components: {
+    Navbar,
+    RoomDetail
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.room {
+  &__image__wrapper {
+    background-image: url('./../assets/image/homePage.jpg');
+    background-position: center;
+    background-size: cover;
+    width: 100%;
+    height: 500px;
+    position: relative;
+  }
+  &__information__item {
+    display: flex;
+    justify-content: center;
+    background-color: #F1F1F1;
+    text-align: center;
+  }
+  &__information__button {
+    width: 110px;
+    margin: 0 20px;
+    padding: 14px 10px 12px 10px;
+    cursor: pointer;
+
+    &:hover {
+      border-bottom: 1.5px solid;
+    }
+  }
+  &__information__link {
+    text-decoration: none;
+    color: #000000;
+  }
+}
+</style>
