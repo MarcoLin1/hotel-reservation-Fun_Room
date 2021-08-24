6286d10449c920f6bb481d7c0df9f570918abc76
Marco Lin <pohanlin0605@gmail.com>
2021-08-24 17:33:56 +0800

    feat: add confirm view

---
+++ src/views/Confirm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="confirm__page__container">
+    <Navbar v-if="false"/>
+    <ConfirmDetail />
+  </div>
+</template>
+
+<script>
+import Navbar from './../components/Navbar.vue'
+import ConfirmDetail from './../components/ConfirmDetail.vue'
+export default {
+  components: {
+    Navbar,
+    ConfirmDetail
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
